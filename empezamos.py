40345451ace482dea107ec9b74b1038adc4bda40
KyoMisael <124635884+KyoMisael@users.noreply.github.com>
2023-03-22 13:30:54 -0300

    Add files via upload

---
+++ empezamos.py
@@ -0,0 +1,37 @@
+#suma, resta, multiplicacion y division..
+print("presiona un numero luego presiona ENTER: ")
+n_1 = int(input())
+print("presiona otro numero: ")
+n_2 = int(input())
+n_3 = 5
+suma = n_1 + n_2
+resta = n_1 - n_2
+multi = n_1 * n_2
+divi = n_1 / n_2
+
+#Los nombres deben tener String"", los numero no es necesario..
+madre = "abuela"
+padre = "abuelo"
+
+print("la suma es:",suma )
+print("la resta es:", resta)
+print("la multiplicacion es:",multi)
+print("la division es:",divi)
+print(n_3)
+
+n1 = 25
+n2 = 26
+n3 = n1 <= n2
+n4 = 25 <= 26
+print (n4)
+print (n3)
+print(n3 != n4)
+numeros_en_letra = "nome"
+letras_en_numero = "quemes"
+print(f"lo vi por tv y dice; {numeros_en_letra + letras_en_numero}")
+if n_1 <= n_2:
+    print(n_3)
+letras = len(numeros_en_letra + letras_en_numero)
+print(f"La palabra {numeros_en_letra + letras_en_numero} tiene {letras} letras")
+print(len(madre))
+print(madre)
